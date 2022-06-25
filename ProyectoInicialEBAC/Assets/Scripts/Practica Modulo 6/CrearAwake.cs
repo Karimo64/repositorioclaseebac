baf51f1480e9bd44920c2cb537326ec391fdd120
KarimoSP <KarimSP_98@outlook.com>
2022-06-25 02:11:15 -0500

    Correccion Tarea Modulo 6
    Se realizaron cambios en el codigo, para simplificarlo.

--- ProyectoInicialEBAC/Assets/Scripts/Practica Modulo 6/CrearAwake.cs
+++ ProyectoInicialEBAC/Assets/Scripts/Practica Modulo 6/CrearAwake.cs
@@ -4,42 +4,19 @@ using UnityEngine;
 
 public class CrearAwake : MonoBehaviour
 {
-    GameObject objToSpawn;
-    Vector3[] vertices = {
-        new Vector3 (0,0,0),
-        new Vector3 (1,0,0),
-        new Vector3 (1,1,0),
-        new Vector3 (0,1,0),
-        new Vector3 (0,1,1),
-        new Vector3 (1,1,1),
-        new Vector3 (1,0,1),
-        new Vector3 (0,0,1),
-    };
-
-    int[] triangulos = {
-        0, 2, 1,
-        0, 3, 2,
-        2, 3, 4,
-        2, 4, 5,
-        1, 2, 5,
-        1, 5, 6,
-        0, 7, 4,
-        0, 4, 3,
-        5, 4, 7,
-        5, 7, 6,
-        0, 6, 7,
-        0, 1, 6
-    };
+    public GameObject objToSpawn;
+   
 
     private void Awake() {
-        objToSpawn = new GameObject("Nuestro Primer Cubo");
+        /*
+        Instantiate(objToSpawn);
+        objToSpawn = new GameObject("Random Color Cube");
+        
 
         //Agregar MeshFilter 
         objToSpawn.AddComponent<MeshFilter>();
         var meshFilter = objToSpawn.GetComponent<MeshFilter>().mesh;
         meshFilter.Clear();
-        meshFilter.vertices = vertices;
-        meshFilter.triangles = triangulos;
         meshFilter.Optimize();
         meshFilter.RecalculateNormals();
 
@@ -58,7 +35,17 @@ public class CrearAwake : MonoBehaviour
 
         //Posicion donde se genera
         objToSpawn.transform.position = Vector3.one;
+        */
+        GameObject cuboSpawn = Instantiate<GameObject>(objToSpawn);
+
+        var materialRend = objToSpawn.GetComponent<MeshRenderer>().material;
+        materialRend.color = Color.white;
+
+        cuboSpawn.GetComponent<MeshRenderer>().material.color = materialRend.color;
+    
+        cuboSpawn.transform.position= new Vector3(1f,1.25f,0f);
     }
+
     // Start is called before the first frame update
     void Start()
     {
