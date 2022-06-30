2bcf1efc0ff16cffb3ff04df81ddce7c738fe3c6
KarimoSP <KarimSP_98@outlook.com>
2022-06-30 00:15:20 -0500

    Update VariablesCharYString.cs

--- ProyectoInicialEBAC/Assets/Scripts/VariablesCharYString.cs
+++ ProyectoInicialEBAC/Assets/Scripts/VariablesCharYString.cs
@@ -18,20 +18,20 @@ public class VariablesCharYString : MonoBehaviour
 
         /////////////////////////////////////////////////////
 
-        char miCaracter; 
-        string miString = "Hola desde EBAC";
-        string miSegundoString = miString.ToUpper();
-        string miTercerString = miString + " " + miSegundoString;
-        //string ejemploEscape = "c:\\Users\\pepek\\Documents";
-        miCaracter = miString[13];
-        string miNombre = "Karim";
-        string misApellidos = "Sosa Perez";
-        string primerApellido = misApellidos.Substring(0,4);
-        string salidasSuma = "mi nombre es: " + miNombre + "y mis apellidos son " + misApellidos;
-        string salida = $"Minombre es: {miNombre} Y mis Apellidos son {misApellidos}";
-        int longitud = miString.Lenght;
-        Debug.Log(salida);
-        Debug.Log(salidaSuma);
+        // char miCaracter; 
+        // string miString = "Hola desde EBAC";
+        // string miSegundoString = miString.ToUpper();
+        // string miTercerString = miString + " " + miSegundoString;
+        // //string ejemploEscape = "c:\\Users\\pepek\\Documents";
+        // miCaracter = miString[13];
+        // string miNombre = "Karim";
+        // string misApellidos = "Sosa Perez";
+        // string primerApellido = misApellidos.Substring(0,4);
+        // string salidasSuma = "mi nombre es: " + miNombre + "y mis apellidos son " + misApellidos;
+        // string salida = $"Minombre es: {miNombre} Y mis Apellidos son {misApellidos}";
+        // int longitud = miString.Lenght;
+        // Debug.Log(salida);
+        // Debug.Log(salidaSuma);
 
     }
 
